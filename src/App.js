f016cba0e73fe99d07f06b9fea444df30086356f
Heet Doshi <88900807+HS-doshi@users.noreply.github.com>
2022-10-31 01:50:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import useReducer from "./component/Hooks/useReducer";
+// import UseReducer from "./Component/Hooks/useReducer.js"
+// import Todo from "./Component/Todoreact/todo";
+import Todo from "./Component/Todoreact/todo";
+
+const App = () => {
+  return <>
+    <Todo />
+  </>;
+};
+
+export default App;
